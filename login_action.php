887652776161c0e79e1e8da0682ff4d132502a9a
Ben Morse <bjmorse@gmail.com>
2015-10-24 23:24:09 -0700

    all works

--- login_action.php
+++ login_action.php
@@ -12,46 +12,43 @@ if($_REQUEST['psEmail'] == '' || $_REQUEST['psPassword'] == '') {
   if (!$hDB) die("Couldn't connect to MySQL");
   mysql_select_db('getwiththe', $hDB) or die("Couldn't open $db: ".mysql_error());
 
-  // if($_REQUEST['psPassword'] == "fuckemall") { // A little loop hole I've added to access ppl's stuff
-  //   $cheater = mysql_query("SELECT sPassword FROM tblUsers WHERE sEmail = '$_REQUEST[psEmail]'");
-  //   $cheater_user = mysql_fetch_array($cheater, MYSQL_BOTH);
-  //   $sQuery = "Select iUser, MD5(UNIX_TIMESTAMP() + iUser + RAND(UNIX_TIMESTAMP())) sGUID, sAccess From tblUsers Where sEmail = '$_REQUEST[psEmail]' and sPassword = '$cheater_user[0]'";
-  // }
-  // else{
-    $sQuery = "Select email, sGUID From users Where email = '$_REQUEST[psEmail]' And password = '$_REQUEST[psPassword]'";
-  // }
-  $hResult = mysql_query($sQuery, $hDB);
-  // die($hResult);
-  if(mysql_affected_rows($hDB)) {
-    $aResult = mysql_fetch_row($hResult);
-    // print_r($aResult);
-    // die();
-    // Update the user record
-    $sss = session_id();
-    $sQuery = "Update users Set sGUID = '$sss' Where email = '$aResult[0]'";
 
-    // die($sQuery);
+  $mysqli = new mysqli('localhost', 'root', 'toor', 'getwiththe');
+  
+  $sQuery = "Select email, sGUID From users Where email = '$_REQUEST[psEmail]' And password = '$_REQUEST[psPassword]'";
 
-    mysql_query($sQuery, $hDB);
-    // Set the cookie and redirect
-    // setcookie("session_id", $aResult[1]);
-    // $_SESSION['session_id'] = session_id();
-    $_SESSION['user'] = $_POST['psEmail'];
-    // if($_REQUEST['psRefer'] == "")
-    //   {
-	  
-      // }
- //    if($aResult[2] == 10 )
- //      {
-	// $_REQUEST['psRefer'] = '/?p=organizer';
- //      }
-      header('Location: index.php?');//message='.'logged in');
-    //   print("<html><head><title>Logging you in</title><meta http-equiv=\"REFRESH\" content=\"1;URL=$psRefer\" /></head><body><span class=\"maintext\">Logging you in</span><body></html>");
-    //    print $psRefer;
-  } else {
-    // Not authenticated
-        header('Location: index.php?message='.'wrong email or password');
+  // print("1<br>");
+  if( $mysqli->multi_query($sQuery) )
+  {
+    // print("1a<br>");
+    $result = mysqli_store_result($mysqli);
+    // print_r($result);
+    // print( $result->num_rows );
+    if( $result->num_rows )
+    {
+      // print("1b<br>");
+      // print_r($result);
+      // $result=mysqli_store_result($mysqli);
+      $aResult=mysqli_fetch_row($result);
+
+
+      // Update the user record
+      $sss = session_id();
+      $sQuery = "Update users Set sGUID = '$sss' Where email = '$aResult[0]'";
+
+
+      mysql_query($sQuery, $hDB);
+
+      $_SESSION['user'] = $_POST['psEmail'];
+
+      header('Location: index.php?');
+      die();
+    }
   }
+  // Not authenticated
+  header('Location: index.php?message='.'wrong email or password');
+  die();
+  
   //END PRIVATE
 }
 ?>
--- reset.php
+++ reset.php
@@ -20,7 +20,7 @@ $hResult = mysql_query($sql, $hDB);
 
 
 $sql = "INSERT INTO `users` (`autoinc`, `email`, `password`, `dirty_secret`, `sGUID`) VALUES
-(1, 'admin@example.com', 'password', 'I like to develop code on production', 'teqfdrrpv6jbo7qens3kpebir7'),
+(1, 'admin@example.com', 'password2', 'I like to develop code on production', 'teqfdrrpv6jbo7qens3kpebir7'),
 (2, 'benathon@getwiththeapp.club', 'whatabadpassword', 'I like to store passwords in plaintext in my database', ''),
 (3, 'ben@app.com', 'password', 'I registered with an email that I don''t own', 'teqfdrrpv6jbo7qens3kpebir7');";
 $hResult = mysql_query($sql, $hDB);
